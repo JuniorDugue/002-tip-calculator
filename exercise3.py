38137f8e4058f8e278d6d2ebaa4ca0f5c757f9b6
juniordugue <jrdugue@gmail.com>
2021-01-14 22:26:57 -0500

    topics: quiz on which would fail

---
+++ exercise3.py
@@ -0,0 +1,15 @@
+# name = input("what is your name? ")
+# print(f"Hello, {name}")
+
+# name = input("what is your name? ")
+# print("hello, " + name)
+
+# age = 12
+# print(f"you are {age} years old")
+
+# age = 12
+# print("you are " + age + " years old")
+
+# 12% -> 1.12
+percentage = 100
+print(12 * percentage)
